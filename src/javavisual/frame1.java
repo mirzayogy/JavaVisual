eb7d363f678df33a6b6aa06c52dc146bfb3647f7
Mirza Yogy Kurniawan <mirza.yogy@gmail.com>
2017-10-27 20:13:23 +0800

    membuat constructor

--- src/javavisual/frame1.java
+++ src/javavisual/frame1.java
@@ -1,5 +1,12 @@
 package javavisual;
 
-public class frame1 {
-    
+import javax.swing.JFrame;
+
+public class frame1 extends JFrame{
+    public frame1(){
+        setTitle("Ini loh TITLE");
+        setSize(600,400);
+        setLocation(100,200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
