900e5f0c65ce7bcc6f5a8a2312f0d7c9fb5eaa7c
Mirza Yogy Kurniawan <mirza.yogy@gmail.com>
2017-10-27 20:18:20 +0800

    menjalankan method tampil()

--- src/javavisual/JavaVisual.java
+++ src/javavisual/JavaVisual.java
@@ -3,6 +3,7 @@ public class JavaVisual {
     public static void main(String[] args) {
         // TODO code application logic here
         frame1 f = new frame1();
+        f.tampil();
     }
     
 }
